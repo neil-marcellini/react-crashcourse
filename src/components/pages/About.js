04add9656f926ddf9046cd2584900ab47993ee5f
neil-marcellini <neil.marcellini@gmail.com>
2020-10-23 17:59:29 -0700

    adding router and links in header

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+       <React.Fragment>
+           <h1>About</h1>
+           <p>TodoList App v1.0.0 for React Crashcourse</p>
+        </React.Fragment>
+    )
+}
+export default About;
